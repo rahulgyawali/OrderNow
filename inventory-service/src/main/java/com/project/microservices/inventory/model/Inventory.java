e45d9f3370e7ea0a756835a29ef834207d8fdd46
rahulgyawali <rgyawal1@asu.edu>
2024-07-09 22:32:17 -0700

    Added inventory service

---
+++ inventory-service/src/main/java/com/project/microservices/inventory/model/Inventory.java
@@ -0,0 +1,19 @@
+package com.project.microservices.inventory.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "t_inventory")
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,5 @@
+insert into t_inventory(quantity,sku_code)
+values  (100, 'iphone_15'),
+        (200, 'galaxy_s_22'),
+        (300, 'pixel_7'),
+        (400, 'one_plus_12');
